b6c2b39cc57eb9812d14f7b4e05272cb4b69eaab
Parth Patil <parth.patil@email.ucr.edu>
2019-07-29 13:22:54 -0700

    bash scripting

---
+++ bashScripts/our_script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Hello World!"
+mkdir ~/Desktop/magic1
+cd ~/Desktop/magic1
+touch file{1..2}.txt
+ls -lh ~/Desktop/magic1 > ~/Desktop/magic/logs.log
---
+++ bashScripts/our_script1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Hello World!"
+mkdir ~/Desktop/magic
+cd ~/Desktop/magic
+touch file{1..2}.txt
+#ls -lh ~/Desktop/magic > ~/Desktop/magic.log
